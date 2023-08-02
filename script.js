bd6b9fc23f88054d8683052b1bb963a9392e4f15
George-2612 <129542917+George-2612@users.noreply.github.com>
2023-08-02 17:13:52 -0300

    commit init

---
+++ script.js
@@ -0,0 +1,25 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+    menu.classList.remove('active');
+}
+
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    menu.classList.toggle('active');
+    search.classList.remove('active');
+}
+//Hide menu and search box on scroll
+window.onscroll = () => {
+    menu.classList.remove('active');
+    search.classList.remove('active');
+}
+
+// header
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
